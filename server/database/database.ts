4a45dd5ef88a314d69b76ac163ab0596c9369490
FThierryDev <97753490+FThierryDev@users.noreply.github.com>
2023-06-07 14:30:27 -0300

    Configurando o banco para rodar no docker e no atlas

--- server/database/database.ts
+++ server/database/database.ts
@@ -8,6 +8,7 @@ main().catch((err) => console.log(err));
 
 async function main() {
   await mongoose.connect(mongoDBUri);
+  // await mongoose.connect(`mongodb://127.0.0.1:27017/EventLocation`);
 
   console.log("Database Connected");
 }
