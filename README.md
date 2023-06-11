61ade7aa8c72d9a68806b2649c1811a9f0618757
RICHARD WESLEY BARBOSA FREITAS <wesleyrichard30@gmail.com>
2023-06-11 16:00:02 -0300

    adicionando readme e implementando redis em tudo

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# Academic-Maps
- 
+## Getting Started
+
+1. Install the dependencies in the ./web and ./server directory with:
+```bash
+npm i
+```
+2. Create an ``.env`` file in the ./server directory with this ambient-variables:
+```bash
+MONGODB_URI = 'mongodb connection link' # Here goes the connection link with mongodb, can be an external or local connection.
+REDIS_URL = 'redis connection link' # Here goes redis external link, but this is optional, if you don't want to use an external connection, you can leave this field empty to use the local redis.
+URL_PERM = 'cors restriction link' # Here goes the access restriction to the origin of cors, but this is optional, if you don't want to restrict the cors, you can leave this field empty.
+```
+3. Create an ``.env.local`` file in the ./web directory with this ambient-variables:
+```bash
+NEXT_PUBLIC_MAP_KEY = 'api maps key' # Here goes the google, maps api-key, but this is optional, you can leave this field empty.
+NEXT_PUBLIC_URL_API = 'api server link' # Here goes the back-end api link, default is http://localhost:8080, if you change default port or use web deployment port, you change this.
+```
+4. Run the api server and web server, first navigate to the ./server directory and then to the ./web directory and run the following command:
+```bash
+npm run dev
+```
+5. Open [http://localhost:3000](http://localhost:3000) with your browser to see the frent-end server.
--- server/src/controllers/EventController.ts
+++ server/src/controllers/EventController.ts
@@ -30,18 +30,29 @@ export class EventController {
     const { value } = req.query;
     try {
       if (value) {
+        const redisConsult = await Redis.get(`search-${value}`);
+        if (redisConsult) return res.json(JSON.parse(redisConsult));
         const events = await Event.find(
           { $text: { $search: `%${value}%` } },
           { _id: true, __v: false },
         );
-        if (events.length > 0) return res.json(events);
+        if (events.length > 0) {
+          await Redis.set(`search-${value}`, JSON.stringify(events), { EX: 3600 });
+          return res.json(events);
+        } else return res.sendStatus(204);
+      }
+      const redisConsult = await Redis.get('event-items');
+      if (redisConsult) return res.json(JSON.parse(redisConsult));
+      else {
+        const events = await Event.find({}, { _id: true, __v: false }).sort({
+          startDate: -1,
+        });
+        if (events.length > 0) {
+          await Redis.set(`event-items`, JSON.stringify(events), { EX: 3600 });
+          return res.json(events);
+        }
         else return res.sendStatus(204);
       }
-      const events = await Event.find({}, { _id: true, __v: false }).sort({
-        startDate: -1,
-      });
-      if (events.length > 0) return res.json(events);
-      else return res.sendStatus(204);
     } catch (error) {
       return res.sendStatus(404);
     }
@@ -50,11 +61,11 @@ export class EventController {
   public async findEvent(req: Request, res: Response) {
     const id = req.params.id;
     try {
-      const redisConsult = await Redis.get(id);
+      const redisConsult = await Redis.get(`id-${id}`);
       if (redisConsult) return res.json(JSON.parse(redisConsult));
       const event = await Event.findById(id);
       if (event) {
-        await Redis.set(id, JSON.stringify(event), { EX: 3600 });
+        await Redis.set(`id-${id}`, JSON.stringify(event), { EX: 3600 });
         return res.json(event);
       }
     } catch (error) {
