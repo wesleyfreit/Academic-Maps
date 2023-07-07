a01c65cffc94a699252e8580790d4fb6269bb973
FThierryDev <97753490+FThierryDev@users.noreply.github.com>
2023-07-07 17:34:20 -0300

    Atualizando arquivo de rotas

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,14 +1,19 @@
 import { Router } from "express";
 import { EventController } from "./controllers/EventController";
+import { UserController } from "./controllers/UserController";
 
 const router = Router();
 
 const eventController = new EventController();
+const userController = new UserController();
 
 router.get("/events", eventController.listEvents);
 router.get("/events/:id", eventController.findEvent);
 router.post("/events", eventController.saveEvent);
 router.put("/events/:id", eventController.updateEvent);
 router.delete("/events/:id", eventController.deleteEvent);
+
+router.post("/signup", userController.signUp);
+router.post("/signin", userController.singIn);
 
 export default router;
