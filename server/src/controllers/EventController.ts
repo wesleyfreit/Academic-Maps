0d30fe1af95ccd492a30812bce61ee2b81009eb1
FThierryDev <97753490+FThierryDev@users.noreply.github.com>
2023-07-07 22:06:17 -0300

    Atualizando controller de Eventos

--- server/src/controllers/EventController.ts
+++ server/src/controllers/EventController.ts
@@ -1,6 +1,9 @@
-import { Request, Response } from 'express';
-import Event from '../models/Event';
-import Redis from '../../database/redis';
+import { Request, Response } from "express";
+import Event from "../models/Event";
+import Redis from "../../database/redis";
+import { Neo } from "./NeoController";
+
+const neo = new Neo();
 
 type iEvent = {
   title: string;
@@ -16,11 +19,16 @@ type iEvent = {
 export class EventController {
   public async saveEvent(req: Request, res: Response) {
     const body = <iEvent>req.body;
-    if (body.title != '' && body.point.coordinates[1] && body.point.coordinates[0]) {
+    if (
+      body.title != "" &&
+      body.point.coordinates[1] &&
+      body.point.coordinates[0]
+    ) {
       try {
         const event = await Event.create(body);
         await Redis.set(`id-${event.id}`, JSON.stringify(event), { EX: 3600 });
-        await Redis.del('array-events');
+        await Redis.del("array-events");
+        await neo.saveEvent(event.id);
         return res.sendStatus(201);
       } catch (error) {
         return res.sendStatus(400);
@@ -34,18 +42,18 @@ export class EventController {
       if (value) {
         const events = await Event.find(
           { $text: { $search: `%${value}%` } },
-          { _id: true, __v: false },
+          { _id: true, __v: false }
         );
         if (events.length > 0) return res.json(events);
         else return res.sendStatus(204);
       }
-      const redisEvents = await Redis.get('array-events');
+      const redisEvents = await Redis.get("array-events");
       if (redisEvents) return res.json(JSON.parse(redisEvents));
       const events = await Event.find({}, { _id: true, __v: false }).sort({
         startDate: -1,
       });
       if (events.length > 0) {
-        await Redis.set('array-events', JSON.stringify(events), { EX: 3600 });
+        await Redis.set("array-events", JSON.stringify(events), { EX: 3600 });
         return res.json(events);
       } else return res.sendStatus(204);
     } catch (error) {
@@ -71,13 +79,17 @@ export class EventController {
   public async updateEvent(req: Request, res: Response) {
     const body = <iEvent>req.body;
     const id = req.params.id;
-    if (body.title != '' && body.point.coordinates[1] && body.point.coordinates[0]) {
+    if (
+      body.title != "" &&
+      body.point.coordinates[1] &&
+      body.point.coordinates[0]
+    ) {
       try {
         const result = await Event.findOneAndUpdate({ _id: id }, { ...body });
         if (result) {
           const event = await Event.findById(id);
           await Redis.set(`id-${id}`, JSON.stringify(event), { EX: 3600 });
-          await Redis.del('array-events');
+          await Redis.del("array-events");
           return res.sendStatus(200);
         } else return res.sendStatus(400);
       } catch (error) {
@@ -92,7 +104,7 @@ export class EventController {
       const result = await Event.findOneAndRemove({ _id: id });
       if (result) {
         await Redis.del(`id-${id}`);
-        await Redis.del('array-events');
+        await Redis.del("array-events");
         return res.sendStatus(200);
       } else return res.sendStatus(400);
     } catch (error) {
