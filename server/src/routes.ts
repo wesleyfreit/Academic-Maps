2c4db8b2a06a9891d8c4ce5c31498b6f2f8ded08
FThierryDev <97753490+FThierryDev@users.noreply.github.com>
2023-07-07 22:07:42 -0300

    criando rotas de subscrição

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import { EventController } from "./controllers/EventController";
 import { UserController } from "./controllers/UserController";
+import { Neo } from "./controllers/NeoController";
 
 const router = Router();
 
 const eventController = new EventController();
 const userController = new UserController();
+const neo = new Neo();
 
 router.get("/events", eventController.listEvents);
 router.get("/events/:id", eventController.findEvent);
@@ -15,5 +17,8 @@ router.delete("/events/:id", eventController.deleteEvent);
 
 router.post("/signup", userController.signUp);
 router.post("/signin", userController.singIn);
+
+router.get("/subscribe/:id", neo.subscribe);
+router.get("/subscribed/:id", neo.findSubscribeds);
 
 export default router;
