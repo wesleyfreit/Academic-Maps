3e2ddf653e1edcdc89cee97e1e5ea416247abfe0
RICHARD WESLEY BARBOSA FREITAS <wesleyrichard30@gmail.com>
2023-07-09 18:09:08 -0300

    adicionando opção de signout

--- web/src/components/Menu/UserTab.tsx
+++ web/src/components/Menu/UserTab.tsx
@@ -1,9 +1,21 @@
+'use client'
+
 import { LogOut } from 'lucide-react';
-import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 import React from 'react';
 
 export default function UserTab(props: { username: string }) {
   const { username } = props;
+  const router = useRouter()
+
+  const logout = async () => {
+    const decision = confirm("Você tem certeza que deseja sair?");
+    if(decision){
+      router.push('/api/signout');
+      alert('Sua conta foi desconectada.');
+      router.refresh();
+    }
+  }
   return (
     <>
       <div
@@ -11,13 +23,13 @@ export default function UserTab(props: { username: string }) {
         className="absolute bg-gray-600 p-2 pr-5 rounded shadow-black shadow-md mt-[.6rem] -ml-2 cursor-auto"
       >
         <p className="font-bold text-xl mb-2">{username}</p>
-        <Link
+        <button
           title="Sair da Conta"
-          href={'/api/signout'}
+          onClick={logout}
           className="text-red-600 hover:font-bold hover:text-red-500 "
         >
           <LogOut className="w-5 h-5" />
-        </Link>
+        </button>
       </div>
     </>
   );
