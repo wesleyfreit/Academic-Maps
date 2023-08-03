a74de667d8eb97706ffe8fdcf3e0f2c0f18d442a
RICHARD WESLEY BARBOSA FREITAS <wesleyrichard30@gmail.com>
2023-08-03 13:33:24 -0300

    removendo integração do redis com eventscontroller

--- server/src/controllers/EventController.ts
+++ server/src/controllers/EventController.ts
@@ -1,6 +1,5 @@
 import { Request, Response } from 'express';
 import Event from '../models/Event';
-import Redis from '../../database/redis';
 import { Neo } from './NeoController';
 
 const neo = new Neo();
@@ -22,8 +21,6 @@ export class EventController {
     if (body && body.title != '' && body.point.coordinates[1] && body.point.coordinates[0]) {
       try {
         const event = await Event.create(body);
-        await Redis.set(`id-${event.id}`, JSON.stringify(event), { EX: 3600 });
-        await Redis.del('array-events');
         await neo.saveEvent(event.id);
         return res.sendStatus(201);
       } catch (error) {
@@ -43,15 +40,11 @@ export class EventController {
         if (events.length > 0) return res.json(events);
         else return res.sendStatus(204);
       }
-      const redisEvents = await Redis.get('array-events');
-      if (redisEvents) return res.json(JSON.parse(redisEvents));
       const events = await Event.find({}, { _id: true, __v: false }).sort({
         startDate: -1,
       });
-      if (events.length > 0) {
-        await Redis.set('array-events', JSON.stringify(events), { EX: 3600 });
-        return res.json(events);
-      } else return res.sendStatus(204);
+      if (events.length > 0) return res.json(events);
+      else return res.sendStatus(204);
     } catch (error) {
       return res.sendStatus(404);
     }
@@ -60,13 +53,8 @@ export class EventController {
   public async findEvent(req: Request, res: Response) {
     const id = req.params.id;
     try {
-      const redisConsult = await Redis.get(`id-${id}`);
-      if (redisConsult) return res.json(JSON.parse(redisConsult));
       const event = await Event.findById(id);
-      if (event) {
-        await Redis.set(`id-${id}`, JSON.stringify(event), { EX: 3600 });
-        return res.json(event);
-      }
+      if (event) return res.json(event);
     } catch (error) {
       return res.sendStatus(404);
     }
@@ -79,9 +67,7 @@ export class EventController {
       try {
         const result = await Event.findOneAndUpdate({ _id: id }, { ...body });
         if (result) {
-          const event = await Event.findById(id);
-          await Redis.set(`id-${id}`, JSON.stringify(event), { EX: 3600 });
-          await Redis.del('array-events');
+          await Event.findById(id);
           return res.sendStatus(200);
         } else return res.sendStatus(400);
       } catch (error) {
@@ -94,11 +80,8 @@ export class EventController {
     const id = req.params.id;
     try {
       const result = await Event.findOneAndRemove({ _id: id });
-      if (result) {
-        await Redis.del(`id-${id}`);
-        await Redis.del('array-events');
-        return res.sendStatus(200);
-      } else return res.sendStatus(400);
+      if (result) return res.sendStatus(200);
+      else return res.sendStatus(400);
     } catch (error) {
       return res.sendStatus(404);
     }
